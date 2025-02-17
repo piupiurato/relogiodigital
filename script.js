3fc6f59f5b816d87ecbcbfd3a5421286bf72a5cd
piupiurato <64154292+piupiurato@users.noreply.github.com>
2025-02-17 03:05:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function atualizarRelogio() {
+    let agora = new Date();
+    let horas = agora.getHours().toString().padStart(2, "0");
+    let minutos = agora.getMinutes().toString().padStart(2, "0");
+    let segundos = agora.getSeconds().toString().padStart(2, "0");
+
+    document.getElementById("relogio").textContent = `${horas}:${minutos}:${segundos}`;
+}
+
+setInterval(atualizarRelogio, 1000);
+atualizarRelogio(); // Atualiza imediatamente ao carregar a página
